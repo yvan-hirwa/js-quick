d6a85d5ee515cdb0c88a196f8b83232c98207c94
hirwkar <hirwakaranganwayvan@gmail.com>
2024-06-28 20:45:00 +0200

    moved addToCart function to cart file and exported using modules

--- projects/finalproject-Amazon-like-App/data/cart.js
+++ projects/finalproject-Amazon-like-App/data/cart.js
@@ -1,1 +1,25 @@
 export const cart=[]
+
+export function addToCart(productId){
+
+    let matchingItem;
+    //const quantityContainer= Number(document.querySelector('.js-quantity-container').value);
+    cart.forEach((cartItem)=>{
+        if (productId===cartItem.productId) {
+            matchingItem= cartItem;
+        }
+    });                                 // checking of the item exist in cart and adding on the quantity
+
+    if (matchingItem) {
+        matchingItem.quantity++;
+    } else {
+        cart.push({
+        productId,
+        quantity: 1
+    });
+    }
+    //console.log(document.querySelector('.js-quantity-container'));
+    //calculating the cart total quantity
+    // consideration of cart counting => let cartQuantity= cart.length;
+
+}
