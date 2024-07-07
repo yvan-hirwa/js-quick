f0a98cb558ddc7228d192dc0a032f94679aee16d
hirwkar <hirwakaranganwayvan@gmail.com>
2024-07-07 19:36:49 +0200

    re-arranged order chekout page files

--- projects/finalproject-Amazon-like-App/scripts/index.js
+++ projects/finalproject-Amazon-like-App/scripts/index.js
@@ -1,4 +1,4 @@
-import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
+import { addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
