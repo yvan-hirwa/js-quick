720b544ba09232edbf0b0589513a39a6eab15c51
hirwkar <hirwakaranganwayvan@gmail.com>
2024-07-08 19:01:57 +0200

    used MVC on delete link

--- projects/finalproject-Amazon-like-App/scripts/checkout/orderSummary.js
+++ projects/finalproject-Amazon-like-App/scripts/checkout/orderSummary.js
@@ -134,8 +134,7 @@ export function renderOrderSummary(){
                 const productId= link.dataset.deleteLink
                 removeFromCart(productId);
 
-                const container = document.querySelector(`.js-cart-item-container-${productId}`);
-                container.remove();
+                renderOrderSummary();
                 renderPaymentSummary();
             })
         })
