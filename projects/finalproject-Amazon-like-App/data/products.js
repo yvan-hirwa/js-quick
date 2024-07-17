e9ad126dbcff8dd9c2a5193d3b72ade1e1ee15c2
hirwkar <hirwakaranganwayvan@gmail.com>
2024-07-17 23:06:06 +0200

    re structuring the products array

--- projects/finalproject-Amazon-like-App/data/products.js
+++ projects/finalproject-Amazon-like-App/data/products.js
@@ -10,6 +10,49 @@ export function getProduct(productId){
   return matchingCartItem;
 }
 
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  keywords;
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return this.priceCents;
+  }
+}
+
+  let productTest = new Product({ 
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  });
+
+  console.log(productTest);
 
 
 export const products = [
@@ -671,4 +714,8 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+  return new Product(productDetails);
+});
+
+console.log(products); 
