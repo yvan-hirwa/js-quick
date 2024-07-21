5dcbe65cc602610e1bec295e7e7dffbd2cf56c81
hirwkar <hirwakaranganwayvan@gmail.com>
2024-07-21 23:58:50 +0200

    sent http request

--- projects/finalproject-Amazon-like-App/data/backend-practice.js
+++ projects/finalproject-Amazon-like-App/data/backend-practice.js
@@ -1,3 +1,8 @@
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://supersimplebackend.dev/');
+
+xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+})
+
+xhr.open('GET', 'https://supersimplebackend.dev/images/apple.jpg');
 xhr.send();
