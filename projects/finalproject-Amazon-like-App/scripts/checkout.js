d01c23806a42d8e5aa4c785c3c3203c787b2c564
hirwkar <hirwakaranganwayvan@gmail.com>
2024-07-25 15:01:11 +0200

    learned promises

--- projects/finalproject-Amazon-like-App/scripts/checkout.js
+++ projects/finalproject-Amazon-like-App/scripts/checkout.js
@@ -4,7 +4,20 @@ import { loadProducts } from "../data/products.js";
 //import '../data/car.js';
 //import '../data/backend-practice.js';
 
+
+new Promise((resolve) =>{
+    loadProducts(()=>{
+
+        resolve();
+    })
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+
+/*
 loadProducts(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 })
+    */
